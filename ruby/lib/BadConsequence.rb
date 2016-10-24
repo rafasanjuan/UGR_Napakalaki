83c037f2b87c00e963c002f76893c6161440ed8d
Less <rafaelsanjuanaguilera@gmail.com>
2016-10-24 18:09:53 +0200

    Adding ruby

---
+++ ruby/lib/BadConsequence.rb
@@ -0,0 +1,9 @@
+class BadConsequence
+  def initialize( text, levels, nVisibleTreasures, nHiddenTreasures, death )
+    @text = text
+    @levels = levels
+    @nVisibleTreasures = nVisibleTreasures
+    @nHiddenTreasures = nHiddenTreasures
+    @death = death
+  end
+end
---
+++ ruby/lib/TreasureKind.rb
@@ -0,0 +1,10 @@
+module Napakalaki
+  
+    ARMOR =     :ARMOR
+    ONEHAND =   :ONEHAND
+    BOTHHAND =  :BOTHHAND
+    HELMET =    :HELMET
+    SHOE =      :SHOE
+    NECKLACE =  :NECKLACE
+  
+end
