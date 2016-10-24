6cf68c789ed4440b5f047094759f682eb1a8f3d2
Less <rafaelsanjuanaguilera@gmail.com>
2016-10-24 18:52:35 +0200

    BadConsequence updated

--- ruby/lib/Prize.rb
+++ ruby/lib/Prize.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 class Prize
   
   attr_reader :treasures, :levels
@@ -5,6 +7,10 @@ class Prize
   def initialize( treasures, levels )
     @treasures = treasures
     @levels = levels
+  end
+  
+  def to_s
+  "Tesoros ganados: #{@treasures} \n Niveles ganados: #{@level}"
   end
   
 end
--- ruby/lib/TreasureKind.rb
+++ ruby/lib/TreasureKind.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 module Napakalaki
   
     ARMOR =     :ARMOR
