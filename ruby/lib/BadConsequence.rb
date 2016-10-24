8b7360479e5da5c4fc67c31fabef794d16a594d8
Less <rafaelsanjuanaguilera@gmail.com>
2016-10-24 18:31:14 +0200

    mistakes were made

--- ruby/lib/BadConsequence.rb
+++ ruby/lib/BadConsequence.rb
@@ -1,9 +1,26 @@
 class BadConsequence
-  def initialize( text, levels, nVisibleTreasures, nHiddenTreasures, death )
+  
+  attr_reader :text, :levels, :nVisibleTreasures, :nHiddenTreasures, :specificVisibleTreasures, :specificHiddenTreasures, :death
+  
+  def initialize( text, levels, nVisibleTreasures, nHiddenTreasures, specificVisibleTreasures= Array.new, specificHiddenTreasures= Array.new, death )
     @text = text
     @levels = levels
     @nVisibleTreasures = nVisibleTreasures
     @nHiddenTreasures = nHiddenTreasures
+    @specificVisibleTreasures = specificVisibleTreasures
+    @specificHiddenTreasures = specificHiddenTreasures
     @death = death
   end
+  
+  def self.new_level_number_of_treasures (aText, someLevels, someVisibleTreasures, someHiddenTreasures)
+      new(aText, someLevels, someVisibleTreasures, someHiddenTreasures)
+  end
+    
+  def self.new_level_specific_treasures (aText, someLevels, someSpecificVisibleTreasures, someSpecificHiddenTreasures)
+      new(aText,someLevels,0, 0, someSpecificVisibleTreasures, someSpecificHiddenTreasures)
+  end
+    
+  def self.new_death (aText)
+      new( aText, 0, 0, 0, Array.new, Array.new, true )
+  end  
 end
--- ruby/lib/Prize.rb
+++ ruby/lib/Prize.rb
@@ -1,12 +1,10 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
 class Prize
   
-  attr_reader :level, :treasures 
+  attr_reader :treasures, :levels
   
-  def initialize( levels,  )
-    
+  def initialize( treasures, levels )
+    @treasures = treasures
+    @levels = levels
   end
+  
 end
