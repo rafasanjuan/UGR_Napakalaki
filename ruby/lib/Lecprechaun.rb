d0ab4773c5a3133a353c4e7ff69ae2209813ed28
Less <rafaelsanjuanaguilera@gmail.com>
2016-11-06 12:39:13 +0100

    Añadido examen de la practica 1

---
+++ ruby/lib/Lecprechaun.rb
@@ -0,0 +1,39 @@
+# Examen practica 1.
+#encoding: utf-8
+
+class Leprechaun
+
+  attr_reader :name
+  private_class_method :new
+  
+  def initialize( aName, aGoldpot )
+    @name = aName
+    @goldpot = aGoldpot
+  end
+  
+  def self.newName ( aName )
+      new( aName, [] )
+  end
+    
+  def self.newNameAndGoldpot ( aName, aGoldpot )
+      new( aName, aGoldpot )
+  end
+  
+  def addTreasureKind( treasure_kind )
+    @goldpot.push( treasure_kind )
+  end
+  
+  def to_s
+    tiene_zapatos = false
+    for i in 0...@goldpot.size 
+      if @goldpot[i] == TreasureKind::SHOE and tiene_zapatos == false then
+        "Oh yes!"
+        tiene_zapatos = true
+      end
+      if tiene_zapatos == false then
+        "Oh no!"
+      end
+    end
+  end
+  
+end
